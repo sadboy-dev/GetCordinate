f05cdb35253d93d5c2a3fd2873adf1ac9f7a713b
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2025-10-15 22:27:59 +0800

    Update singa.lua

--- singa.lua
+++ singa.lua
@@ -1,5 +1,5 @@
 return {
-CFrame.new(149.74, 905.35, 209.31) * CFrame.Angles(0,0,0),
 CFrame.new(170.09, 889.82, 149.50) * CFrame.Angles(0,0,0),
-CFrame.new(170.86, 887.59, 148.02) * CFrame.Angles(0,0,0)
+CFrame.new(170.86, 887.59, 148.02) * CFrame.Angles(0,0,0),
+CFrame.new(149.74, 905.35, 209.31) * CFrame.Angles(0,0,0)
 }
