6f8c6ae978fea1a6e7b8d8c2235b2686a7209929
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2025-10-18 15:18:21 +0800

    Create mika.lua

---
+++ mika.lua
@@ -0,0 +1,5 @@
+return {
+CFrame.new(471.14, 962.14, 2063.28) * CFrame.Angles(0,0,0),
+CFrame.new(470.50, 962.14, 2067.27) * CFrame.Angles(0,0,0),
+CFrame.new(471.94, 962.14, 2074.73) * CFrame.Angles(0,0,0)
+}
