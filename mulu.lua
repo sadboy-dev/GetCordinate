0c8401ff6cb2da5d6cf1b82f2761e37c7ae87cd3
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2025-10-23 08:59:34 +0800

    Add new CFrame positions to mulu.lua

--- mulu.lua
+++ mulu.lua
@@ -1,4 +1,7 @@
 return {
+CFrame.new(-661.36, 2053.10, -861.41) * CFrame.Angles(0,0,0),
+CFrame.new(-665.00, 2053.10, -859.60) * CFrame.Angles(0,0,0),
+CFrame.new(-666.88, 2053.10, -856.65) * CFrame.Angles(0,0,0),
 CFrame.new(580.03, 2464.42, -894.68) * CFrame.Angles(0,0,0),
 CFrame.new(581.77, 2464.95, -894.62) * CFrame.Angles(0,0,0),
 CFrame.new(587.00, 2467.71, -894.50) * CFrame.Angles(0,0,0)
