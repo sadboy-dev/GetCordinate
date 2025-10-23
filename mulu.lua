b24d6da8247baa31545be279da8312be2311fe8b
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2025-10-23 08:45:51 +0800

    Add CFrame positions to return statement

--- mulu.lua
+++ mulu.lua
@@ -1,4 +1,5 @@
-return{
-
-
+return {
+CFrame.new(580.03, 2464.42, -894.68) * CFrame.Angles(0,0,0),
+CFrame.new(581.77, 2464.95, -894.62) * CFrame.Angles(0,0,0),
+CFrame.new(587.00, 2467.71, -894.50) * CFrame.Angles(0,0,0)
 }
